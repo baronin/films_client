943a6a51ed0a0d06131c7ce9d9956e2182e55092
romannikiforov <escaro@ukr.net>
2021-08-06 20:38:19 +0300

    start lesson 2

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { Component } from "react";
+import { prop, sortWith, ascend, descend } from "ramda";
+import FilmsList from "pages/FilmsPage/components/FilmsList";
+import { films } from "data";
+import FilmContext from "contexts/FilmContext";
+
+class App extends Component {
+  state = {
+    films: [],
+  };
+
+  sortFilms = (films) =>
+    sortWith([descend(prop("featured")), ascend(prop("title"))], films);
+
+  componentDidMount() {
+    this.setState({ films: this.sortFilms(films) });
+  }
+
+  toggleFeatured = (id) =>
+    this.setState(({ films }) => ({
+      films: this.sortFilms(
+        films.map((f) => (f._id === id ? { ...f, featured: !f.featured } : f))
+      ),
+    }));
+
+  value = {
+    toggleFeatured: this.toggleFeatured,
+  };
+
+  render() {
+    const { films } = this.state;
+    return (
+      <FilmContext.Provider value={this.value}>
+        <div className="ui container">
+          <FilmsList films={films} />
+        </div>
+      </FilmContext.Provider>
+    );
+  }
+}
+
+export default App;
