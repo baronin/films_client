9f69106de076bacd170454916f357a6ce576b2a3
romannikiforov <escaro@ukr.net>
2021-08-06 20:49:28 +0300

    add code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const name = "Bill";
+typeof name === "strng";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (!"serviceWorker" in navigator) {
+  // you have an old browser
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const greeting = "Hello";
+console
+  .log("${greeting} world")
+
+  [(1, 2, 3)].forEach((x) => console.log(x));
