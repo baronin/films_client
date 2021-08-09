2d560a08df0dedb1ffc70390cc71223debca3302
romannikiforov <escaro@ukr.net>
2021-08-09 19:32:31 +0300

    reasy Card.jsx from scratch

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import FilmCard from "pages/FilmsPage/components/FilmCard";
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <FilmCard />
+    </div>
+  );
+};
+
+export default App;
