f145228eb1677609995f01e8f246891168120e99
Yaroslav <baronin.yaroslav@gmail.com>
2021-08-11 09:23:00 +0300

    completed homework

--- src/pages/FilmsPage/components/FilmCard.jsx
+++ src/pages/FilmsPage/components/FilmCard.jsx
@@ -1,31 +1,47 @@
-import PropTypes from "prop-types";
-import Featured from "components/Featured";
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Featured from 'components/Featured';
 
 const FilmCard = ({ film }) => {
+  console.log(film);
+  const [isShowFilm, setIsShowFilm] = useState(false);
+  const showDescriptiopn = () => {
+    setIsShowFilm(!isShowFilm);
+  };
   return (
-    <div className="ui card">
+    <div className='ui card'>
       <Featured film={film} />
-      <div className="image">
-        <span className="ui green label ribbon">$ {film.price} </span>
-        <img src={film.img} alt={film.title} />
-      </div>
+      {isShowFilm ? (
+        <div className='content'>
+          <p>{film.description}</p>
+        </div>
+      ) : (
+        <div className='image'>
+          <span className='ui green label ribbon'>$ {film.price} </span>
+          <img src={film.img} alt={film.title} />
+        </div>
+      )}
 
-      <div className="content">
-        <span className="header">{film.title}</span>
-        <div className="meta">
-          <i className="icon users"></i> {film.director}
-          <span className="right floated">
-            <i className="icon wait right"></i> {film.duration} min
+      <div className='content'>
+        <span className='header'>{film.title}</span>
+        <div className='meta'>
+          <i className='icon users'></i> {film.director}
+          <span className='right floated'>
+            <i className='icon wait right'></i> {film.duration} min
           </span>
         </div>
+        <i
+          className={`icon eye link ${isShowFilm ? 'slash' : ''}`}
+          onClick={showDescriptiopn}
+        ></i>
       </div>
-      <div className="extra content">
-        <div className="ui two buttons">
-          <span className="ui green basic button">
-            <i className="ui icon edit"></i>
+      <div className='extra content'>
+        <div className='ui two buttons'>
+          <span className='ui green basic button'>
+            <i className='ui icon edit'></i>
           </span>
-          <span className="ui red basic button">
-            <i className="ui icon trash"></i>
+          <span className='ui red basic button'>
+            <i className='ui icon trash'></i>
           </span>
         </div>
       </div>
