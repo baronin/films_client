a907efe487c4e396c127edd7b8e09a81398cd2a6
roman <escaro@ukr.net>
2021-08-16 19:53:55 +0300

    ready TopNavigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,30 @@ import { Component } from "react";
 import { Route } from "react-router-dom";
 import TopNavigation from "components/TopNavigation";
 import HomePage from "pages/HomePage";
+
+
 import FilmsPage from "pages/FilmsPage";
 
+
+
+
+const initUser = {
+  token: null,
+  role: "user",
+};
+
 class App extends Component {
+  state = {
+    user: initUser,
+  };
+
+  logout = () => this.setState({ user: { token: null } });
+
   render() {
+    const { user } = this.state;
     return (
       <div className="ui container">
-        <TopNavigation />
+        <TopNavigation logout={this.logout} isAuth={!!user.token} />
         <Route exact path="/">
           <HomePage />
         </Route>
--- src/components/TopNavigation.jsx
+++ src/components/TopNavigation.jsx
@@ -1,7 +1,7 @@
 import { memo } from "react";
 import { NavLink } from "react-router-dom";
 
-const TopNavigation = () => {
+const TopNavigation = ({ isAuth, logout }) => {
   return (
     <div className="ui secondary pointing menu">
       <NavLink exact to="/" className="item">
@@ -14,6 +14,23 @@ const TopNavigation = () => {
       <NavLink exact to="/films/new" className="item">
         <i className="icon plus" /> Add new film
       </NavLink>
+
+      <div className="right menu">
+        {isAuth ? (
+          <span onClick={logout} className="item">
+            <i className="icon sign-out" /> Logout
+          </span>
+        ) : (
+          <>
+            <NavLink to="/signup" className="item">
+              <i className="icon sign-in" /> Signup
+            </NavLink>
+            <a to="/login" className="item">
+              <i className="icon user" /> Login
+            </a>
+          </>
+        )}
+      </div>
     </div>
   );
 };
